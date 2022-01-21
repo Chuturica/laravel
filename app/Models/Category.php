09141bb656c43b4bd0682bfc83b366a70ce3f5b6
Chuturica <chuturica@gmail.com>
2022-01-21 12:31:50 +0100

    End of Section 4

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    public function posts()
+    {
+        # code...
+        // hasOne, hasManny, belongsTo, belongsToMeny
+        return $this->hasMany(Post::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title','excerpt','body'];
+    protected $guarded =[];
+
+    protected $with = ['category','author'];
+    //protected $fillable = ['title','excerpt','body'];
+
+    public function category()
+    {
+        # code...
+        // hasOne, hasManny, belongsTo, belongsToMeny
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author()
+    {
+        # code...
+        return $this->belongsTo(User::class,'user_id');
+    }
 }
