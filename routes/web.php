48cb9ef415774e8347f1af49b952cbaeb96ae751
Chuturica <chuturica@gmail.com>
2022-01-26 10:35:12 +0100

    36

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/', function () {
         'posts' => Post::latest()->get(),
         'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 Route::get('/posts/{post:slug}', function (Post $post) {
     // find a post by it slug and pass it to view called "post"
@@ -44,7 +44,7 @@ Route::get('categories/{category:slug}', function(Category $category){
         'categories' => Category::all()
     ]);
 }
-);
+)->name('category');
 
 Route::get('authors/{author:username}', function(User $author){
     return view('posts',[
