32afb65f6f15f8ddb3945c7ec42090eb58458062
Chuturica <chuturica@gmail.com>
2022-01-26 11:16:02 +0100

    39

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Validation\Rule;
 
@@ -9,10 +10,22 @@ class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index', [
-            'posts' => Post::latest()->filter(
-                        request(['search', 'category', 'author'])
-                    )->paginate(18)->withQueryString()
+        // return view('posts.index', [
+        //     'posts' => Post::latest()->filter(
+        //                 request(['search', 'category', 'author'])
+        //             )->paginate(18)->withQueryString()
+        // ]);
+
+        $posts = Post::latest();
+
+        if (request('search')){
+            $posts->where('title', 'like', '%' . request('search') . '%')
+            ->orWhere('body', 'like', '%' . request('search') . '%');
+        }
+
+        return view('posts',[
+            'posts' => $posts->get(),
+            'categories' => Category::all()
         ]);
     }
 
